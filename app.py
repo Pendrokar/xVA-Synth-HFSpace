831426065e4595f63f3e93f68c699184d9d807b3
Yanis Lukes <Pendrokar@gmail.com>
2024-02-11 23:44:21 +0200

    example caching breaks Space

--- app.py
+++ app.py
@@ -119,15 +119,15 @@ gradio_app = gr.Interface(
 		pacing_slider
 	],
 	outputs=gr.Audio(label="22kHz audio", type="filepath"),
-	title="xVASynth (WIP)",
-	examples=[
-		["Once, I headed in much deeper. But I doubt I'll ever do that again.", 1],
-		["You love hurting me, huh?", 1.5],
-		["Ah! What is happening to me?!?", 1],
-		["Embrace your demise!", 1],
-		["Never come back!", 1]
-	],
-	cache_examples=None
+	title="xVASynth (WIP)"
+	# examples=[
+	# 	["Once, I headed in much deeper. But I doubt I'll ever do that again.", 1],
+	# 	["You love hurting me, huh?", 1.5],
+	# 	["Ah! What is happening to me?!?", 1],
+	# 	["Embrace your demise!", 1],
+	# 	["Never come back!", 1]
+	# ],
+	# cache_examples=None
 )
 
 
